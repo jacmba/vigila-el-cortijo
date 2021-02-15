6db2279a2d69d9cacef4d5380a3391d7145034e1
Jacinto Mba Cantero <jacmba@gmail.com>
2021-02-15 17:51:09 +0000

    Inventory operations and tests

---
+++ Assets/Scripts/Game/InventoryManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager
+{
+  public int slots = 10;
+
+  private Dictionary<ItemType, int> itemMap = new Dictionary<ItemType, int>();
+
+  public int getAvailSlots()
+  {
+    return slots - itemMap.Count;
+  }
+
+  public void insert(ItemType item, int amount)
+  {
+    if (amount <= 0)
+    {
+      return;
+    }
+
+    int items;
+    if (itemMap.TryGetValue(item, out items))
+    {
+      itemMap.Remove(item);
+    }
+    else
+    {
+      items = 0;
+    }
+    itemMap.Add(item, items + amount);
+  }
+
+  public int count(ItemType item)
+  {
+    int items;
+    if (itemMap.TryGetValue(item, out items))
+    {
+      return items;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+
+  public int use(ItemType item, int amount)
+  {
+    if (amount <= 0)
+    {
+      return 0;
+    }
+
+    int items;
+    int remain;
+    if (itemMap.TryGetValue(item, out items))
+    {
+      remain = items - amount;
+      if (remain <= 0)
+      {
+        itemMap.Remove(item);
+        return items;
+      }
+      else
+      {
+        itemMap.Remove(item);
+        itemMap.Add(item, remain);
+        return amount;
+      }
+    }
+    else
+    {
+      return 0;
+    }
+  }
+}
---
+++ Assets/Tests/Edit/InventoryTests.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+  public class InventoryTests
+  {
+    // A Test behaves as an ordinary method
+    [Test]
+    public void InventoryTestsCheckObjectBuild()
+    {
+      InventoryManager mgr = new InventoryManager();
+      Assert.IsNotNull(mgr);
+      Assert.AreEqual(mgr.slots, 10);
+    }
+
+    [Test]
+    public void InventoryTestsCheckAvailableSlots()
+    {
+      InventoryManager mgr = new InventoryManager();
+      mgr.slots = 3;
+      int avail = mgr.getAvailSlots();
+      Assert.AreEqual(3, avail);
+    }
+
+    [Test]
+    public void InventoryTestsCheckSlotInsert()
+    {
+      InventoryManager mgr = new InventoryManager();
+      mgr.insert(ItemType.CEPA, 5);
+      int avail = mgr.getAvailSlots();
+      int cepas = mgr.count(ItemType.CEPA);
+      Assert.AreEqual(9, avail);
+      Assert.AreEqual(5, cepas);
+    }
+
+    [Test]
+    public void InventoryTestsCheckNonExistingItemShouldHaveZero()
+    {
+      InventoryManager mgr = new InventoryManager();
+      int waters = mgr.count(ItemType.WATER);
+      Assert.AreEqual(0, waters);
+    }
+
+    [Test]
+    public void InventoryTestsCheckUseItem()
+    {
+      InventoryManager mgr = new InventoryManager();
+      mgr.slots = 2;
+      mgr.insert(ItemType.CEPA, 2);
+      mgr.insert(ItemType.WINE, 5);
+      Assert.AreEqual(0, mgr.getAvailSlots());
+
+      int used = mgr.use(ItemType.CEPA, 1);
+      Assert.AreEqual(1, used);
+      Assert.AreEqual(0, mgr.getAvailSlots());
+      Assert.AreEqual(1, mgr.count(ItemType.CEPA));
+
+      used = mgr.use(ItemType.CEPA, 3);
+      Assert.AreEqual(1, used);
+      Assert.AreEqual(1, mgr.getAvailSlots());
+      Assert.AreEqual(0, mgr.count(ItemType.CEPA));
+
+      used = mgr.use(ItemType.CEPA, 1);
+      Assert.AreEqual(0, used);
+    }
+
+    [Test]
+    public void InventoryTestInsertExistingShouldSum()
+    {
+      InventoryManager mgr = new InventoryManager();
+      mgr.insert(ItemType.CEPA, 1);
+      mgr.insert(ItemType.CEPA, 3);
+
+      Assert.AreEqual(9, mgr.getAvailSlots());
+      Assert.AreEqual(4, mgr.count(ItemType.CEPA));
+    }
+
+    [Test]
+    public void InventoryTestShouldNotAddNegativeValues()
+    {
+      InventoryManager mgr = new InventoryManager();
+      mgr.insert(ItemType.CEPA, -10);
+      Assert.AreEqual(10, mgr.getAvailSlots());
+      Assert.AreEqual(0, mgr.count(ItemType.CEPA));
+    }
+
+    [Test]
+    public void InventoryTestShoudNotUseNegativeAmounts()
+    {
+      InventoryManager mgr = new InventoryManager();
+      mgr.insert(ItemType.CEPA, 10);
+      int used = mgr.use(ItemType.CEPA, -1);
+      Assert.AreEqual(0, used);
+      Assert.AreEqual(9, mgr.getAvailSlots());
+      Assert.AreEqual(10, mgr.count(ItemType.CEPA));
+    }
+  }
+}
