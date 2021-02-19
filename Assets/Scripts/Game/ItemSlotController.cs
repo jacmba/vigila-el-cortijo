a0ce6507d0289e32f8e7761d283767e7bba439ab
Jacinto Mba Cantero <jacmba@gmail.com>
2021-02-19 17:51:19 +0000

    :construction: Slot item behavior implementation and tests

---
+++ Assets/Scripts/Game/ItemSlotController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlotController : MonoBehaviour
+{
+  public InventoryItem item;
+
+  public GameObject infoNode;
+  private Image icon;
+  private Text count;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    infoNode = transform.Find("ItemSlotInfo").gameObject;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+  }
+}
---
+++ Assets/Tests/Play/ItemSlotTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+  public class ItemSlotTests
+  {
+    private ItemSlotController slot;
+
+    [SetUp]
+    public void Setup()
+    {
+      GameObject s = GameObject.Instantiate(Resources.Load("Prefabs/ItemSlot") as GameObject);
+      slot = s.GetComponent<ItemSlotController>();
+    }
+
+    [UnityTest]
+    public IEnumerator ItemSlotTestsObjectBasicCheck()
+    {
+      yield return null;
+
+      Assert.IsNotNull(slot);
+      Assert.IsFalse(slot.infoNode.activeSelf);
+    }
+  }
+}
