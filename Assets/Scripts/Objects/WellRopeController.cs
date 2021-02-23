6d9c4995b808dc5f38e63a38d54e95515faf041a
Jacinto Mba Cantero <jacmba@gmail.com>
2021-02-23 18:49:39 +0000

    Well rope stretching with bucket movement

---
+++ Assets/Scripts/Objects/WellRopeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Draws the rope that links bucket to well
+/// </summary>
+public class WellRopeController : MonoBehaviour
+{
+  /// <summary>
+  /// Reference to bucket position
+  /// </summary>
+  public Transform bucket;
+
+  private LineRenderer line;
+
+  /// <summary>
+  /// Start is called before the first frame update
+  /// </summary>
+  void Start()
+  {
+    line = GetComponent<LineRenderer>();
+  }
+
+  /// <summary>
+  /// Update is called once per frame
+  /// </summary>
+  void Update()
+  {
+    line.SetPosition(0, transform.position);
+    line.SetPosition(1, bucket.position);
+  }
+}
---
+++ Assets/Tests/Play/WellTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+  public class WellTests
+  {
+    private GameObject pozo;
+    private GameObject pozoPref;
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+      pozoPref = Resources.Load<GameObject>("Prefabs/pozo");
+
+      GameObject camera = new GameObject();
+      camera.AddComponent<Camera>();
+      camera.transform.Translate(Vector3.back * 10 + Vector3.up * 3);
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+      pozo = GameObject.Instantiate(pozoPref);
+      pozo.transform.position = Vector3.zero;
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      GameObject.Destroy(pozo);
+    }
+
+    [UnityTest]
+    public IEnumerator WellTestsWithEnumeratorPasses()
+    {
+      yield return null;
+
+      Assert.NotNull(pozo, "Pozo instance should not be null");
+    }
+
+    [UnityTest]
+    public IEnumerator WellTestsRopeShouldFollowBucket()
+    {
+      Transform bucket = pozo.transform.Find("bucket");
+      LineRenderer rope = pozo.GetComponentInChildren<LineRenderer>();
+
+      Vector3 pos = new Vector3(10.000000f, 10.000000f, 10.000000f);
+      bucket.position = pos;
+
+      yield return null;
+
+      Assert.IsTrue(pos == rope.GetPosition(1), "Rope bottom edge should be at {10, 10, 10}");
+    }
+  }
+}
