9ada2fc970a82cc909aa9137627d378b29ad47c8
Jacinto Mba Cantero <jacmba@gmail.com>
2021-03-01 15:42:34 +0000

    Virtual joystick movement

---
+++ Assets/Scripts/Player/MobileInputManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handle input for mobile devices
+/// </summary>
+public class MobileInputManager : IInputManager
+{
+  /// <summary>
+  /// Reference to player controller script
+  /// </summary>
+  public PlayerController player;
+
+  private MobileJoystickController joystickController;
+
+  /// <summary>
+  /// Start is called on the frame when a script is enabled just before
+  /// any of the Update methods is called the first time.
+  /// </summary>
+  void Start()
+  {
+    player.im = this;
+    joystickController = GetComponentInChildren<MobileJoystickController>();
+  }
+
+  /// <summary>
+  /// Update is called every frame, if the MonoBehaviour is enabled.
+  /// </summary>
+  void Update()
+  {
+    h = joystickController.h;
+    v = joystickController.v;
+  }
+}
--- Assets/Scripts/Player/MobileJoystickController.cs
+++ Assets/Scripts/Player/MobileJoystickController.cs
@@ -6,10 +6,70 @@ using UnityEngine.EventSystems;
 /// <summary>
 /// Process mobile virtual joystick input
 /// </summary>
-public class MobileJoystickController : MonoBehaviour, IPointerEnterHandler
+public class MobileJoystickController : IInputManager, IDragHandler, IPointerDownHandler,
+  IPointerUpHandler
 {
-  public void OnPointerEnter(PointerEventData data)
+  /// <summary>
+  /// Maximum pivot distance from origin
+  /// </summary>
+  public float maxDist = 5f;
+
+  /// <summary>
+  /// Joystick drag sentitivity
+  /// </summary>
+  public float step = 10f;
+
+  private RectTransform rectTransform;
+  private Vector2 origin;
+
+  /// <summary>
+  /// Start is called on the frame when a script is enabled just before
+  /// any of the Update methods is called the first time.
+  /// </summary>
+  void Start()
   {
-    Debug.Log("Clicked!!");
+    rectTransform = GetComponent<RectTransform>();
+    origin = rectTransform.pivot;
+  }
+
+  /// <summary>
+  /// Event triggered when joystick is pressed
+  /// </summary>
+  /// <param name="data">Event info</param>
+  public void OnPointerDown(PointerEventData data)
+  {
+    OnDrag(data);
+  }
+
+  /// <summary>
+  /// Event triggered when joystick is released
+  /// </summary>
+  /// <param name="data">Event info</param>
+  public void OnPointerUp(PointerEventData data)
+  {
+    rectTransform.pivot = origin;
+  }
+
+  /// <summary>
+  /// Event triggered when dragging handle
+  /// </summary>
+  /// <param name="data">Dragging information</param>
+  public void OnDrag(PointerEventData data)
+  {
+    Vector2 move = rectTransform.pivot + data.delta * step * Time.deltaTime;
+    float dist = Mathf.Abs((move - origin).magnitude);
+    if (dist <= maxDist)
+    {
+      rectTransform.pivot = move;
+    }
+  }
+
+  /// <summary>
+  /// Update is called every frame, if the MonoBehaviour is enabled.
+  /// </summary>
+  void Update()
+  {
+    h = (rectTransform.pivot.x - origin.x) / maxDist;
+    v = (rectTransform.pivot.y - origin.y) / maxDist;
   }
 }
