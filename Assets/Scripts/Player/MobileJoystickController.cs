ff021d8583c5b9dbb3ca25b85dc71a4295f54c23
Jacinto Mba Cantero <jacmba@gmail.com>
2021-03-01 01:44:24 +0000

    :beer: :construction: hunting pointer events

---
+++ Assets/Scripts/Player/MobileJoystickController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Process mobile virtual joystick input
+/// </summary>
+public class MobileJoystickController : MonoBehaviour, IPointerEnterHandler
+{
+  public void OnPointerEnter(PointerEventData data)
+  {
+    Debug.Log("Clicked!!");
+  }
+}
