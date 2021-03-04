d652da8e1308f5451d430ec7a815af6facaa0ac5
Jacinto Mba Cantero <jacmba@gmail.com>
2021-03-04 18:01:14 +0000

    Increase button size when over

--- Assets/Scripts/UI/MenuButton.cs
+++ Assets/Scripts/UI/MenuButton.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 /// <summary>
 /// Behavior of UI menu butytons
 /// </summary>
-public class MenuButton : MonoBehaviour
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
   [SerializeField]
   private MenuButtonType type;
   private Vector3 size;
   private Vector3 overSize;
   private Vector3 clickSize;
+  private Button button;
 
   /// <summary>
   /// Start is called on the frame when a script is enabled just before
@@ -22,5 +25,24 @@ public class MenuButton : MonoBehaviour
     size = transform.localScale;
     overSize = size * 1.5f;
     clickSize = size * 2f;
+    button = GetComponent<Button>();
+  }
+
+  /// <summary>
+  /// This function is called when the object becomes enabled and active.
+  /// </summary>
+  void OnEnable()
+  {
+    button.Select();
+  }
+
+  public void OnPointerEnter(PointerEventData data)
+  {
+    transform.localScale = overSize;
+  }
+
+  public void OnPointerExit(PointerEventData data)
+  {
+    transform.localScale = size;
   }
 }
