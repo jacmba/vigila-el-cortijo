b743b56754e6605f4968799eed124b3e6cbd5903
Jacinto Mba Cantero <jacmba@gmail.com>
2021-03-14 23:53:27 +0000

    :construction: Item consuming area

---
+++ Assets/Scripts/Interaction/ConsumingArea.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Behavior of area that consumes an item and triggers an action
+/// </summary>
+public class ConsumingArea : MonoBehaviour
+{
+  [SerializeField]
+  private GameController gc;
+
+  [SerializeField]
+  private ItemType type;
+
+  [SerializeField]
+  private SerializeField amount;
+
+  private IInputManager im;
+  private InventoryManager inventory;
+
+  /// <summary>
+  /// Start is called before the first frame update
+  /// </summary>
+  void Start()
+  {
+    im = gc.gameObject.GetComponent<IInputManager>();
+    inventory = InventoryManager.getInstance();
+  }
+}
